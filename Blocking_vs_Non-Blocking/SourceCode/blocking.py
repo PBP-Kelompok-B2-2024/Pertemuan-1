019e68413085788b340d43ea551c0804ead1d927
Hasbii <mhasbiasshidiqi72@gmail.com>
2024-08-30 13:31:05 +0700

    Memperbaiki bug pada source code

--- Blocking_vs_Non-Blocking/SourceCode/blocking.py
+++ Blocking_vs_Non-Blocking/SourceCode/blocking.py
@@ -1,14 +1,22 @@
 import time
+import os
 
 def read_file_blocking(file_path):
-    with open(file_path, 'r') as file:
-        data = file.read()
-        print(f"Konten file: {data}")
+    try:
+        with open(file_path, 'r') as file:
+            data = file.read()
+            print(f"Konten file {file_path}: {data}")
+    except FileNotFoundError:
+        print(f"File {file_path} tidak ditemukan.")
 
 def main_blocking():
+    # Ubah direktori kerja
+    os.chdir(r'D:\SEMESTER 3\Prinsip Bahasa Pemrograman\Pertemuan-1\Blocking_vs_Non-Blocking\SourceCode')
+    
     start_time = time.time()
     for i in range(5):  # Asumsikan ada 5 file yang perlu dibaca
-        read_file_blocking(f'file_{i}.txt')
+        file_path = f'file_{i}.txt'
+        read_file_blocking(file_path)
     end_time = time.time()
     print(f"Total waktu (blocking): {end_time - start_time} detik")
 
--- Blocking_vs_Non-Blocking/SourceCode/non-blocking.py
+++ Blocking_vs_Non-Blocking/SourceCode/non-blocking.py
@@ -1,5 +1,6 @@
 import asyncio
 import time
+import os
 
 async def read_file_non_blocking(file_path):
     with open(file_path, 'r') as file:
@@ -7,6 +8,8 @@ async def read_file_non_blocking(file_path):
         print(f"Konten file: {data}")
 
 async def main_non_blocking():
+    os.chdir(r'D:\SEMESTER 3\Prinsip Bahasa Pemrograman\Pertemuan-1\Blocking_vs_Non-Blocking\SourceCode')
+
     start_time = time.time()
     tasks = []
     for i in range(5):  # Asumsikan ada 5 file yang perlu dibaca
